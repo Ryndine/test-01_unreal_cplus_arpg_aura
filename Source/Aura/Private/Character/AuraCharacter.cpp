5ead9bde7e3f5e85317519ca16809d1ec113877f
Ryan <rcmangeno@gmail.com>
2025-01-22 10:31:19 -1000

    Adding: Setup EnemyInterface for highlighting

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -7,6 +7,7 @@
 
 AAuraCharacter::AAuraCharacter()
 {
+	// Camera settings to allow model to move/rotate independant from camera.
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright Ryan Mangeno
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
