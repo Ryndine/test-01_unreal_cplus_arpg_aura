8686ab1823ff2359decf110e2c9e58133d63903b
Ryan <rcmangeno@gmail.com>
2025-01-26 10:51:24 -1000

    Added test code for health. New: setup widgets and widget controller.

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Ryan Mangeno
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
